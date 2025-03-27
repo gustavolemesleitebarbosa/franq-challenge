6965857bf9685f7f108c78741f0fed0344ed63db
Gustavo <pregustavomium@gmail.com>
2025-03-27 16:17:59 -0300

    chore: create finance store

---
+++ src/store/financeStore.ts
@@ -0,0 +1,36 @@
+import {
+  CurrencyWithAcronym,
+  FinanceAPIResponse,
+  StockWithAcronym,
+} from "@/types/finance";
+import { create } from "zustand";
+
+type FinanceStore = {
+  currencies: CurrencyWithAcronym[];
+  stocks: StockWithAcronym[];
+  error: string | null;
+  loading: boolean;
+  responseHistory: FinanceAPIResponse[];
+  setCurrencies: (currencies: CurrencyWithAcronym[]) => void;
+  setStocks: (stocks: StockWithAcronym[]) => void;
+  setError: (error: string | null) => void;
+  setLoading: (loading: boolean) => void;
+  setResponseCache: (data: FinanceAPIResponse) => void;
+};
+
+export const useFinanceStore = create<FinanceStore>((set) => ({
+  currencies: [],
+  stocks: [],
+  error: null,
+  loading: true,
+  responseHistory: [],
+  setCurrencies: (currencies) => set({ currencies }),
+  setStocks: (stocks) => set({ stocks }),
+  setError: (error) => set({ error }),
+  setLoading: (loading) => set({ loading }),
+  setResponseCache: (data) => {
+    set((state) => ({
+      responseHistory: [...state.responseHistory, data],
+    }));
+  },
+}));
