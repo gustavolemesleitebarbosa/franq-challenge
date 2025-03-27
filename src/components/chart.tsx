c7150a5190f156902194bc03b90eb2879ed19672
Gustavo <pregustavomium@gmail.com>
2025-03-27 16:21:32 -0300

    feat: create chart component

---
+++ src/components/chart.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+export function PriceChart({
+  labels,
+  dataSet1,
+  dataSet2,
+}: {
+  labels: string[];
+  dataSet1: number[];
+  dataSet2?: number[];
+}) {
+  console.log("labels", labels);
+  console.log("dataSet1", dataSet1);
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: dataSet1,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: dataSet2,
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  return <Line options={options} data={data} />;
+}
