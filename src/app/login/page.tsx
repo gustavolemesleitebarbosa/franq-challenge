83dced3432c65a45140c87aed9557d2d001f3a2f
Gustavo <pregustavomium@gmail.com>
2025-03-28 17:44:17 -0300

    feat: update image container

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,16 +9,15 @@ export default function LoginPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-5 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <div className="mb-4 flex items-center justify-center">
-          <Image
-            priority
-            layout="intrinsic"
-            src="/assets/logo.png"
-            height={129}
-            width={129}
-            alt="Franq Logo"
-          />
-        </div>
+        <Image
+          priority
+          layout="intrinsic"
+          src="/assets/logo.png"
+          height={129}
+          width={129}
+          alt="Franq Logo"
+          className="mb-4"
+        />
         <LoginForm />
       </div>
     </div>
