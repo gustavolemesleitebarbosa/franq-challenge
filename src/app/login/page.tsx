f2e58f425b780ee912c4695331cb959d88d1673a
Gustavo <pregustavomium@gmail.com>
2025-03-27 21:36:18 -0300

    feat: add assets

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,18 @@
 import { LoginForm } from "@/components/login-form";
-
+import Image from "next/image";
 export default function LoginPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-5 p-6 md:p-10">
       <div className="w-full max-w-sm">
+        <div className="flex items-center justify-center mb-4">
+          <Image
+            priority
+            src="/assets/logo.png"
+            height={129}
+            width={129}
+            alt="Franq Logo"
+          />
+        </div>
         <LoginForm />
       </div>
     </div>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,19 @@
 import { RegisterForm } from "@/components/register-form";
+import Image from "next/image";
 
 export default function RegisterPage() {
   return (
     <div className="flex h-screen w-full items-center justify-center font-[family-name:var(--font-geist-sans)]">
       <div className="w-full max-w-sm p-4">
+        <div className="flex justify-center items-center mb-4" >
+          <Image
+            priority
+            src="/assets/logo.png"
+            height={129}
+            width={129}
+            alt="Franq Logo"
+          />
+        </div>
         <RegisterForm />
       </div>
     </div>
